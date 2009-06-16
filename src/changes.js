ce584a862b5c50756e0576ea1f32f63f584273de
DonJinwooo <donjinwooo@gmail.com>
2009-06-16 14:20:09 -0400

    Fix: minor adjustment #29

--- src/changes.js
+++ src/changes.js
@@ -56,3 +56,5 @@ const change_26 = true;
 const change_27 = true;
 // Commit #28: Generated at 2009-06-16T18:02:06
 const change_28 = true;
+// Commit #29: Generated at 2009-06-16T14:20:09
+const change_29 = true;
