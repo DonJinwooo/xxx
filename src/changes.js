6665244dbf6975dd810b3af976177365a9d48258
DonJinwooo <donjinwooo@gmail.com>
2011-08-12 09:40:31 -0400

    Fix: minor adjustment #210

--- src/changes.js
+++ src/changes.js
@@ -418,3 +418,5 @@ const change_207 = true;
 const change_208 = true;
 // Commit #209: Generated at 2011-08-10T10:54:32
 const change_209 = true;
+// Commit #210: Generated at 2011-08-12T09:40:31
+const change_210 = true;
