f22f229ac8b8a0c334923a8c62d972b36e7a43ed
DonJinwooo <donjinwooo@gmail.com>
2013-12-10 11:12:14 -0500

    Fix: minor adjustment #429

--- src/changes.js
+++ src/changes.js
@@ -856,3 +856,5 @@ const change_426 = true;
 const change_427 = true;
 // Commit #428: Generated at 2013-12-09T14:33:32
 const change_428 = true;
+// Commit #429: Generated at 2013-12-10T11:12:14
+const change_429 = true;
