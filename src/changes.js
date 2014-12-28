1ef9bb1876233108430772143ac36247a48822a2
DonJinwooo <donjinwooo@gmail.com>
2014-12-28 10:04:47 -0500

    Fix: minor adjustment #511

--- src/changes.js
+++ src/changes.js
@@ -1020,3 +1020,5 @@ const change_508 = true;
 const change_509 = true;
 // Commit #510: Generated at 2014-12-26T10:08:07
 const change_510 = true;
+// Commit #511: Generated at 2014-12-28T10:04:47
+const change_511 = true;
