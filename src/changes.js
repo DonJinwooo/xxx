328bb717992413860b01fb9ab326a6083b13a28f
DonJinwooo <donjinwooo@gmail.com>
2016-04-25 17:57:07 -0400

    Fix: minor adjustment #624

--- src/changes.js
+++ src/changes.js
@@ -1246,3 +1246,5 @@ const change_621 = true;
 const change_622 = true;
 // Commit #623: Generated at 2016-04-17T17:21:10
 const change_623 = true;
+// Commit #624: Generated at 2016-04-25T17:57:07
+const change_624 = true;
