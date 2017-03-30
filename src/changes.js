e6e92c3b8109e18bd09dbd634bef266657983624
DonJinwooo <donjinwooo@gmail.com>
2017-03-30 13:30:35 -0400

    Fix: minor adjustment #685

--- src/changes.js
+++ src/changes.js
@@ -1368,3 +1368,5 @@ const change_682 = true;
 const change_683 = true;
 // Commit #684: Generated at 2017-03-29T09:02:25
 const change_684 = true;
+// Commit #685: Generated at 2017-03-30T13:30:35
+const change_685 = true;
